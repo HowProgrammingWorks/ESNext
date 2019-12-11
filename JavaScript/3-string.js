d39888ea631dcd3d1a4447eda95d47394c573525
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2019-12-11 02:33:34 +0200

    Add String.prototype methods

---
+++ JavaScript/3-string.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// String.prototype.padStart(targetLength [, padString])
+console.dir('hello'.padStart(20, '.'));
+
+// String.prototype.padEnd(targetLength [, padString])
+console.dir('hello'.padEnd(20, '.'));
+
+// String.prototype.trimStart()
+console.dir('   hello   '.trimStart());
+
+// String.prototype.trimEnd()
+console.dir('   hello   '.trimEnd());
